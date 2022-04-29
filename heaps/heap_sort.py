29985b24fe336e60a5c65e32f846368206956f4b
Elly Wong <elly.yywong@gmail.com>
2022-04-29 12:07:31 -0700

    finished heap_sort

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,3 +1,4 @@
+from heaps.min_heap import MinHeap
 
 
 def heap_sort(list):
@@ -5,4 +6,15 @@ def heap_sort(list):
         Time Complexity:  ?
         Space Complexity: ?
     """
-    pass
+    
+    min_heap = MinHeap()
+    
+    for num in list:
+        min_heap.add(num)
+    
+    i = 0
+    while min_heap.empty() is False:
+        cur_min = min_heap.remove() # remove the root and re-heap the heap
+        list[i] = cur_min
+        i += 1
+    return list 
