f7730b8798e450d52de3e1a870fe99948fb6ab55
Michelle Bodart <michelle.bodart@gmail.com>
2022-05-05 16:57:18 -0700

    first ommit

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,18 @@
-
+from . import min_heap as h
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
         Time Complexity:  ?
         Space Complexity: ?
     """
-    pass
+    heap = h.MinHeap(list)
+    for item in list:
+        heap.add(item)
+
+    for item in list:
+        list.append(heap.remove())
+
+    return list
+
+
+
