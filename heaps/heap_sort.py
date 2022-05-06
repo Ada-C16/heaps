82980d45daae9661c748fb67ea2cfe128d06d5a8
Lia Gaetano <lgaetano19@gmail.com>
2022-05-06 07:30:39 -0700

    Finish heaps

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,23 @@
+from heapq import heappush, heappop
 
-
-def heap_sort(list):
+def heap_sort(lst):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity: O(n logn)
+        Space Complexity: O(n)
     """
-    pass
+    heap = []
+
+    # heappush takes care of the swapping behind the scenes
+    for item in lst: #n log(n)
+        heappush(heap, item)
+
+    # new list
+    ordered = []
+
+    # heappop also takes care of swapping
+    while len(heap) > 0:
+        val = heappop(heap) #n log(n)
+        ordered.append(val)
+
+    return ordered
+
