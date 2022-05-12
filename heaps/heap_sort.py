72d3542ec7c04735b32afae50ac344740647c6be
Jessica <jessica.w.roberts@gmail.com>
2022-05-12 14:00:21 -0700

    Added code to pass tests

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,20 @@
 
 
+from heaps.min_heap import MinHeap
+
+
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(n log n)
+        Space Complexity: O(n)
     """
-    pass
+    heap = MinHeap()
+
+    for item in list:
+        heap.add(item)
+    
+    i = 0
+    while not heap.empty():
+        list[i] = heap.remove()
+        i += 1
+    return list
