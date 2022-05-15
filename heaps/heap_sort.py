a7bd87cf4c409a0b7195eaddf1d8f01d748bad1b
Mac <mackenziespeyton@gmail.com>
2022-05-15 13:10:46 -0700

    feat: add heap_sort functionality

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,19 @@
-
+from heapq import heappush, heappop
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
         Time Complexity:  ?
         Space Complexity: ?
     """
-    pass
+    heap = []
+
+    for item in list: 
+        heappush(heap, item)
+
+    ordered = []
+
+    while len(heap) > 0:
+        value = heappop(heap)
+        ordered.append(value)
+
+    return ordered
