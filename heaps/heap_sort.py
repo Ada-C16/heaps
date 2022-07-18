b5df6a87c5f4acd17470ff4fa04c552392bdb7ac
kaitlyngore <kaitlyngore@gmail.com>
2022-07-18 13:28:13 -0400

    add time space complexity

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -3,8 +3,8 @@ from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(n * n log n)
+        Space Complexity: O(n)
     """
     if len(list) <= 1:
         return list
