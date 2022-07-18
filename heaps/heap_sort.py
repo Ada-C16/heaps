36b79063037a61ae8d0b46c4c6f1431ac9e6088c
kaitlyngore <kaitlyngore@gmail.com>
2022-07-18 13:27:17 -0400

    small change

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -8,13 +8,15 @@ def heap_sort(list):
     """
     if len(list) <= 1:
         return list
-        
+
     heap = MinHeap()
 
     for item in list:
         heap.add(item)
 
     return_list = []
-    while not heap.empty():
+    while heap.empty() != True:
         return_list.append(heap.remove())
+
+    return return_list
 
