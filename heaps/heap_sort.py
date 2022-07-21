2ff87c3d1b706690159000b44d5d771daf3932f1
sjolivas <sjolivas@gmail.com>
2022-07-20 20:01:32 -0600

    Passing all Tests!

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,4 +1,4 @@
-
+from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
@@ -7,3 +7,15 @@ def heap_sort(list):
     """
     if len(list) <= 1:  # returns list of empty or only has one node
         return list
+
+    heap = MinHeap()
+
+    for value in list:
+        heap.add(value)
+
+    index = 0
+    while not heap.empty():
+        list[index] = heap.remove()
+        index += 1
+
+    return list
